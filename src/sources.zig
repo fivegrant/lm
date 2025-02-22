94ba382f07a380681ad391c9a15e0ddce2a57228
Five Grant <5@fivegrant.com>
2025-02-22 15:32:22 -0600

    Add TinyStories as a source

---
+++ src/sources.zig
@@ -0,0 +1,6 @@
+const TINY_STORIES = "https://datasets-server.huggingface.co/rows?dataset=roneneldan%2FTinyStories&config=default&split=train&offset=0&length=100";
+// features
+// rows
+// num_rows_total
+// num_rows_per_page
+// partial
